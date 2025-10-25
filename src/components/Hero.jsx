be593fcc924ca3d987c572876f577a8a8d8d60ac
Arhamirfan <arham.irfan4@gmail.com>
2025-10-25 21:30:34 +0500

    added mavbook4 model switch section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useRef } from 'react'
+import {useEffect, useRef} from "react";
 
 const Hero = () => {
-    let videoRef = useRef();
+    const videoRef = useRef();
 
-    useEffect(()=>{
-        if(videoRef.current) 
-            videoRef.current.playbackRate = 2;
+    useEffect(() => {
+        if(videoRef.current) videoRef.current.playbackRate = 2;
+    }, []);
 
-    },[])
+    return (
+        <section id="hero">
+            <div>
+                <h1>MacBook Pro</h1>
+                <img src="/title.png" alt="MacBook Title" />
+            </div>
 
-  return (
-    <section id='hero'>
-        <div>
-            <h1>MacBook Pro</h1>
-            <img src="/title.png" alt="MacBook title" />
-        </div>
-        <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline/>
-        <button>Buy</button>
-        <p>From $1599 or $1399/mon for 12 months</p>
-    </section>
-  )
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+
+            <button>Buy</button>
+
+            <p>From $1599 or $133/mo for 12 months</p>
+        </section>
+    )
 }
-
 export default Hero
